236a7a4e909833fc11b9877c57d1bc6a5732e8aa
ec98 <carlospiruch98@gmail.com>
2024-01-18 21:24:07 -0500

    estilos css examen final sumativa 1

---
+++ src/components/Imagen.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <img :src="urlImg" alt="No se muestra imagen" />
+    <h1>{{ texto }}</h1>
+  </div>
+</template>
+  
+  <script>
+export default {
+  props: {
+    texto: {
+      type: String
+    },
+    urlImg: {
+      type: String
+    }
+  }
+};
+</script>
+  
+<style scoped>
+.container {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  width: 150px;
+  height: 200px;
+  padding-left: 430px;
+}
+h1 {
+  font-size: 20px;
+  padding-left: 200px;
+}
+</style>
